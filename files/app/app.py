1b790909fb3ac9083c8ff43c2a2244cc6778bfc4
alexmbarbosa <alexmendes.unix@gmail.com>
2022-12-11 21:41:24 +0000

    Fix haproxy conf communication. It works.

--- files/app/app.py
+++ files/app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello from Flask application!'
+def index():
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
